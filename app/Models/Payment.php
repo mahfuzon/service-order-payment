737fb475accf50a3493eb3d2df171051cb2d105f
Mahfuzon Akhiar <mahfuzon0@gmail.com>
2021-08-07 21:49:06 +0700

    setup migration dan model order dan payment

---
+++ app/Models/Payment.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Payment extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'status', 'payment_type', 'raw_response', 'order_id',
+    ];
+
+    protected $cast = [
+        'created_at' => 'datetime:Y-m-d H:m:s',
+        'updated_at' => 'datetime:Y-m-d H:m:s',
+    ];
+}
